84c0f54b7e71e9935382b6e08de080baca425bff
FrankLef <flefebvre01@hotmail.com>
2021-07-17 14:21:31 -0400

    change main.py

--- src/edgar_etl/__main__.py
+++ src/edgar_etl/__main__.py
@@ -6,6 +6,10 @@ import click
 @click.version_option()
 def main() -> None:
     """EDGAR ETL."""
+    # print("EDGAR ETL")
+    # print("\x1b[1;32mEDGAR ETL\x1b[0m")
+    msg = "EDGAR ETL"
+    click.echo(f"\x1b[1;36mInfo:\x1b[0m \x1b[1;32m{msg}\x1b[0m")
 
 
 if __name__ == "__main__":
